d6c3c99c86e1100db8280800b0214b77618d007b
Sanilal P Sathyan <sanilal@gmail.com>
2024-08-22 09:05:41 +0400

    globals added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
-import "@/styles/globals.css"
 import { Inter as FontSans } from "next/font/google"
 
+import './globals.css'
+
 import { cn } from "@/lib/utils"
+import { Metadata } from "next"
+
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
+
+export const metadata: Metadata = {
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor',
+}
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
@@ -14,11 +22,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   )
